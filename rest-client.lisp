564d9263987aa6c135794b5163f329a97967ee34
Noah Muth <noahmuth@gmail.com>
2016-03-13 00:10:04 -0600

    Factor out request logic into macro.

--- rest-client.lisp
+++ rest-client.lisp
@@ -1,5 +1,5 @@
 (ql:quickload '(drakma
-								cl-json))
+                cl-json))
 
 (defpackage :com.nmuth.rest-client (:use :cl :drakma :cl-json))
 
@@ -7,31 +7,38 @@
 (defparameter *test-body* nil)
 
 (defclass response ()
-	((body :initarg :body :accessor body)
-	 (status :initarg :status :accessor status)
-	 (resolved-from :initarg :from :accessor resolved-from)))
+  ((body :initarg :body :accessor body)
+   (status :initarg :status :accessor status)
+   (resolved-from :initarg :from :accessor resolved-from)))
 
 (defmethod print-object ((object response) stream)
-	(print-unreadable-object (object stream :type t)
-		(with-slots (body status) object
-			(format stream "status: ~d, body: ~a" status body))))
+  (print-unreadable-object (object stream :type t)
+    (with-slots (body status) object
+      (format stream "status: ~d, body: ~a" status body))))
 
 (defun new-response (body status uri)
-	(make-instance 'response
-								 :body body
-								 :status status
-								 :from uri))
+  (make-instance 'response
+                 :body body
+                 :status status
+                 :from uri))
 
-(defun request (url)
-	(multiple-value-bind
-				(raw-body status headers uri response-stream must-close)
-			(drakma:http-request url :want-stream t)
-		(let* ((content-type (cdr (assoc :content-type headers)))
-					 (body (if (search "application/json" content-type)
-										 (json:decode-json-strict response-stream)
-										 raw-body))
-					 (response (new-response body status uri)))
-			(progn
-				(when must-close
-					(close response-stream))
-				response))))
+(defun rest/request (url drakma-args)
+  (let ((drakma-arg-list (concatenate 'list (list url :want-stream t) drakma-args)))
+    (multiple-value-bind (raw-body status headers uri response-stream must-close)
+        (apply #'drakma:http-request drakma-arg-list)
+      (let* ((content-type (cdr (assoc :content-type headers)))
+             (body (if (search "application/json" content-type)
+                   (json:decode-json-strict response-stream)
+                   raw-body))
+             (response (new-response body status uri)))
+        (progn
+          (when must-close
+            (close response-stream))
+          response)))))
+
+(defun rest/get (url)
+  (rest/request url '()))
+
+(defun rest/post (url post-body)
+  (let ((post-body-json (json:encode-json post-body)))
+    (rest/request url (list :contents post-body-json))))
