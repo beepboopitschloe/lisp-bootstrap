8ed22a02a0e74e9632a2bc53254935ba9f92f81f
Noah Muth <noahmuth@gmail.com>
2016-03-16 22:58:30 -0500

    Vastly improve logging in user data script.

--- user-data-script.sh
+++ user-data-script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+LOGFILE=/var/log/user-data-script.log
+ERRFILE=/var/log/user-data-script.err.log
+
 PACKAGES="build-essential git-core curl sbcl nginx"
-sudo apt-get update
-sudo apt-get install -y $PACKAGES
+sudo apt-get update > $LOGFILE 2>$ERRFILE
+sudo apt-get install -y $PACKAGES > $LOGFILE 2> $ERRFILE
 
 # set up SBCL daemon
-git clone https://github.com/nmuth/lisp-bootstrap.git /sbcld
-cp /sbcld/sbcld.conf /etc/init/sbcld.conf
+git clone https://github.com/nmuth/lisp-bootstrap.git /sbcld > $LOGFILE 2> $ERRFILE
+cp /sbcld/sbcld.conf /etc/init/sbcld.conf > $LOGFILE 2>$ERRFILE
 
-service sbcld start
+service sbcld start > $LOGFILE 2>$ERRFILE
